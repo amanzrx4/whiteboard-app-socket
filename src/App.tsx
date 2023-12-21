65931e2c623b81f25766525046b0fb8b3543b926
aman <amansagar262@gmail.com>
2023-12-22 03:06:50 +0530

    chore: add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useRef } from "react"
 import "./App.css"
 import Toolbar from "./components/Toolbar"
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   return (
     <main>
       <Toolbar />
+      <canvas
+        id="whiteboard"
+        ref={canvasRef}
+        className="h-[80vh] w-[8-vh] border border-1 border-gray-400 mx-auto rounded-xl"
+      />
     </main>
   )
 }
